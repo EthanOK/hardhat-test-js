bbd6b202a8649a1d0fe020219efae26c8b769015
EthanOk <sdzdj01@163.com>
2023-03-29 18:04:33 +0800

    modified:   contracts/exchange/ProxyAdmin.sol

--- contracts/exchange/ProxyAdmin.sol
+++ contracts/exchange/ProxyAdmin.sol
@@ -2,7 +2,7 @@
 // OpenZeppelin Contracts v4.4.1 (proxy/transparent/ProxyAdmin.sol)
 
 pragma solidity ^0.8.0;
-import "./TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
