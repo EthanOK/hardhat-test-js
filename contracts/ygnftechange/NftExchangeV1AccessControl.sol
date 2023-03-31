558382b467401d9aea94fed8eb33170bfd5c3c87
EthanOk <sdzdj01@163.com>
2023-03-31 18:06:45 +0800

    new file:   contracts/ygnftechange/ExchangeDomainV1.sol

--- contracts/ygnftechange/NftExchangeV1AccessControl.sol
+++ contracts/ygnftechange/NftExchangeV1AccessControl.sol
@@ -9,77 +9,9 @@ import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "./ExchangeDomainV1.sol";
 
-abstract contract ExchangeDomainV1 {
-    enum AssetType {
-        ETH,
-        ERC20,
-        ERC1155,
-        ERC721,
-        ERC721Deprecated
-    }
-
-    struct Asset {
-        address token;
-        uint256 tokenId;
-        AssetType assetType;
-    }
-
-    struct OrderKey {
-        /* who signed the order */
-        address owner;
-        /* random number */
-        uint256 salt;
-        /* what has owner */
-        Asset sellAsset;
-        /* what wants owner */
-        Asset buyAsset;
-    }
-
-    struct Order {
-        OrderKey key;
-        /* The quantity the seller wants to sell */
-        uint256 sellAmount;
-        /* unit price */
-        uint256 unitPrice;
-        // oeder startTime
-        uint256 startTime;
-        // oeder endTime
-        uint256 endTime;
-    }
-
-    /* An ECDSA signature. */
-    struct Sig {
-        /* v parameter */
-        uint8 v;
-        /* r parameter */
-        bytes32 r;
-        /* s parameter */
-        bytes32 s;
-    }
-
-    // Multi Call
-    struct Call {
-        address target;
-        bytes callData;
-    }
-
-    event Buy(
-        address indexed sellToken,
-        uint256 indexed sellTokenId,
-        uint256 sellAmount,
-        uint256 unitPrice,
-        address seller,
-        address buyToken,
-        uint256 buyTokenId,
-        address buyer,
-        uint256 amount,
-        uint256 payPrice,
-        uint256 royaltyFee
-    );
-}
-
-contract NftExchangeV1Upgradeable01 is
+contract NftExchangeV1AccessControl is
     ExchangeDomainV1,
     Initializable,
     AccessControlUpgradeable,
@@ -87,6 +19,12 @@ contract NftExchangeV1Upgradeable01 is
     PausableUpgradeable
 {
     using SafeERC20 for IERC20;
+
+    // Multi Call
+    struct Call {
+        address target;
+        bytes callData;
+    }
 
     bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
@@ -99,7 +37,7 @@ contract NftExchangeV1Upgradeable01 is
     uint256 public platformFee;
 
     constructor() {
-        // _disableInitializers();
+        _disableInitializers();
     }
 
     function initialize(
@@ -230,7 +168,7 @@ contract NftExchangeV1Upgradeable01 is
             royaltyFee
         );
 
-        emit Buy(
+        emit Exchange(
             order.key.sellAsset.token,
             order.key.sellAsset.tokenId,
             order.sellAmount,
