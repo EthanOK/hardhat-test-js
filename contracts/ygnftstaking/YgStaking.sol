cb23b93182f8ed3cae1f9c413410f2fa92b25af0
EthanOk <sdzdj01@163.com>
2023-04-01 09:44:08 +0800

    modified:   contracts/ygnftstaking/YgStaking.sol

--- contracts/ygnftstaking/YgStaking.sol
+++ contracts/ygnftstaking/YgStaking.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./YgStakingDomain.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -9,7 +10,6 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "./YgStakingDomain.sol";
 
 contract YgStaking is YgStakingDomain, ERC721Holder, ReentrancyGuard {
     constructor() {}
