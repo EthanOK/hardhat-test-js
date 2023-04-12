82b10885c77de57b1e5dd87d923a472fa4fa2709
EthanOk <sdzdj01@163.com>
2023-04-12 14:05:00 +0800

    new file:   scripts/deployNftExchangeV2Upgradeable.js

---
+++ scripts/deployNftExchangeV2Upgradeable.js
@@ -0,0 +1,29 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const NFTExchange = await hre.ethers.getContractFactory(
+    "NftExchangeV2Upgradeable"
+  );
+  const nftExchange = await NFTExchange.deploy();
+
+  await nftExchange.deployed();
+
+  console.log(`NFTExchange deployed to ${nftExchange.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+// npx hardhat run scripts/deployNftExchangeV2Upgradeable.js --network goerli
+// npx hardhat verify --network goerli `contractAddress` `args`
+// npx hardhat verify --network goerli 0xDf85f4489b4a4a24BAc2218F88f301CeF70eC5bb
