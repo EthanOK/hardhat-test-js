019ade905fd5ecd8bf7d157f690099cf43e15fd9
EthanOk <sdzdj01@163.com>
2023-04-18 16:52:54 +0800

    new file:   scripts/getTokenIdsOp.js

--- scripts/getTokenIdsOfContract.js
+++ scripts/getTokenIdsOfContract.js
@@ -1,11 +1,12 @@
 const getData = require("../utils/getTokenIdsOfContract.js");
+// mainurl 0xd774557b647330C91Bf44cfEAB205095f7E6c367
+const mnftAddress = "0xd774557b647330C91Bf44cfEAB205095f7E6c367";
+const gnftAddress = "0xEAAfcC17f28Afe5CdA5b3F76770eFb7ef162D20b";
 
-const nftAddress = "0xEAAfcC17f28Afe5CdA5b3F76770eFb7ef162D20b";
-
-// const mainurl = `https://api.etherscan.io`;
+const mainurl = `https://api.etherscan.io`;
 const goerliurl = `https://api-goerli.etherscan.io`;
 
-getData(nftAddress, goerliurl)
+getData(mnftAddress, mainurl)
   .then((data) => {
     console.log(data);
   })
--- scripts/getTokenURI.js
+++ scripts/getTokenURI.js
@@ -3,7 +3,7 @@ const getTokenURI = require("../utils/getTokenURI.js");
 const Multicall2_address = "0x8c6f3bF9Ed05afa8DC0D3f08C2DB4a6E731a3574";
 const nft_address = "0xE29F8038d1A3445Ab22AD1373c65eC0a6E1161a4";
 
-const tokenIds = Array.from({ length: 10 }, (_, i) => i + 1);
+const tokenIds = Array.from({ length: 10000 }, (_, i) => i + 1);
 
 async function main() {
   const result = await getTokenURI(nft_address, tokenIds, Multicall2_address);
