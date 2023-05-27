09a238aea81625e5efcabf03154b67106eb03172
EthanOk <sdzdj01@163.com>
2023-05-27 17:45:40 +0800

    new file:   scripts/getString.js

---
+++ scripts/getString.js
@@ -0,0 +1,9 @@
+let str =
+  "Welcome to Decert!\n\nThis request will not trigger a blockchain transaction or cost any gas fees.\n\nYour authentication status will reset after 24 hours.\n\nWallet address:\n0x0148ff91E06977093992EDdEf76F381E1EE5de2f\n\nNonce:\n154af4eb-efc3-4619-bb26-fb04e1e1acbe";
+
+console.log(str);
+
+let loginMessage =
+  "Welcome. Login YUNGOU NFT exchange Market.\\nThis is completely secure and doesn't cost anything!";
+loginMessage = loginMessage.replace(/\\n/g, "\n");
+console.log(loginMessage);
